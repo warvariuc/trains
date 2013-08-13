689a7245081525d036c04c3c24da8c35edcf6dd6
Victor Varvaryuk <victor.varvariuc@gmail.com>
2013-08-13 18:33:04 +0400

    .

--- trains/admin.py
+++ trains/admin.py
@@ -1,6 +1,7 @@
 __author__ = 'Victor Varvariuc <victor.varvariuc@gmail.com>'
 
 from django.contrib import admin
+from django import forms
 from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
@@ -8,38 +9,33 @@ from django.utils.translation import ugettext_lazy as _
 from .models import Station, Direction, Region, Route, Schedule
 
 
-# http://stackoverflow.com/questions/9919780
-def add_link_field(target_model=None, field='', link_text=_('Редактировать')):
-
-    def add_link(cls):
-        reverse_name = target_model or cls.model.__name__.lower()
-
-        def link(self, instance):
-            app_name = instance._meta.app_label
-            reverse_path = "admin:%s_%s_change" % (app_name, reverse_name)
-            link_obj = getattr(instance, field, None) or instance
-            url = reverse(reverse_path, args=(link_obj.id,))
-            _link_text = link_text(link_obj) if callable(link_text) else link_text
-            return mark_safe("<a href='%s'>%s</a>" % (url, _link_text))
-
-        link.allow_tags = True
-        link.short_description = reverse_name + ' link'
-        cls.link = link
-        cls.readonly_fields = list(getattr(cls, 'readonly_fields', [])) + ['link']
-        return cls
-
-    return add_link
+class RelatedObjectsWidget(forms.widgets.Widget):
+    """Widget showing links to related objects, considering there are not many
+    of them
+    """
+    def render(self, name, value, attrs=None):
+        import ipdb; from pprint import pprint; ipdb.set_trace()
+        return mark_safe('<br/><b>Hello world!</b>')
+        # model = target_model or cls.model
+        # reverse_name = model.__name__.lower()
+        # app_name = model._meta.app_label
+        #
+        # def link(self, instance):
+        #     reverse_path = "admin:%s_%s_change" % (app_name, reverse_name)
+        #     link_obj = getattr(instance, field, None) or instance
+        #     url = reverse(reverse_path, args=(link_obj.id,))
+        #     return mark_safe("<a href='%s'>%s</a>" % (url, _link_text))
 
 
-@add_link_field()
-class DirectionInline(admin.options.InlineModelAdmin):
-    model = Direction
-    template = 'edit_inline.html'
-    extra = 0
+class RegionForm(forms.ModelForm):
+    related_objects = forms.Field(widget=RelatedObjectsWidget())
+    class Meta:
+        model = Region
 
 
+admin.TabularInline
 class RegionAdmin(admin.ModelAdmin):
-    inlines = [DirectionInline]
+    form = RegionForm
 
 
 admin.site.register(Region, RegionAdmin)
